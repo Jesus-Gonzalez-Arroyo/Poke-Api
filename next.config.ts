e3dbaf515992a69bcc0d132ad63421017eeaf6d2
jesus1102 <jesusdavidgonzales1102@gmail.com>
2025-06-28 23:39:52 -0500

    fix(config): configuracion deploy y mejoras de front

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/poke-api',
-  assetPrefix: '/poke-api',
-  distDir: 'docs',  // <-- GitHub Pages sirve desde /docs
-  images: {
-    unoptimized: true // <-- necesario para imágenes estáticas (si usas <Image>)
-  },
-  trailingSlash: true // <-- para que funcione bien en GH Pages
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
